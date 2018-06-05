9ea5567f880236f5c3d96d337e2d6b080b369ceb
bernardo de stefano <bernardo.destefano@gmail.com>
2018-06-05 14:27:27 +0200

    gulp conf improved

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,33 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var path = require('path');
-var sasslint = require('gulp-sass-lint');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const path = require('path');
+const sasslint = require('gulp-sass-lint');
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
 
+const isProduction = process.env.NODE_ENV === 'production';
+
+/** LINT STYLES */
 gulp.task('lint-styles', () =>
   gulp.src('./src/flexy.scss')
     .pipe(sasslint())
     .pipe(sasslint.format()));
 
-gulp.task('styles', ['lint-styles'], () => 
-  gulp.src('./src/flexy.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./dist')));
+/** STYLES */
+gulp.task('styles', ['lint-styles'], () => {
+  const cssBundle = gulp.src('./src/flexy.scss')
+    .pipe(sass().on('error', sass.logError))
+  
+  if(!isProduction) {
+    return cssBundle
+      .pipe(gulp.dest('./dist'))
+  }
+
+  return cssBundle
+    .pipe(cleanCSS())
+    .pipe(rename({ basename: 'flexy.min' }))
+    .pipe(gulp.dest('./dist'));
+});
 
 
 gulp.task('default', ['styles']);
