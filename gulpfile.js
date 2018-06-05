33d55ed5a82014e21a371eebb32ec44b54f5994c
bernardo de stefano <bernardo.destefano@gmail.com>
2018-06-05 16:56:42 +0200

    add browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ const sasslint = require('gulp-sass-lint');
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
 const header = require('gulp-header');
+const browserSync = require('browser-sync');
 
 const pkg = require('./package.json');
 
@@ -57,5 +58,32 @@ gulp.task('styles', ['lint-styles'], () => {
     .pipe(gulp.dest('./dist'));
 });
 
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+       baseDir: "./"
+    }
+  });
+});
 
 gulp.task('default', ['views','styles']);
+
+gulp.task('serve', ['default'], () => {
+  browserSync({
+    server: {
+      baseDir: './',
+      directory: true,
+    },
+    notify: false,
+    port: 4000,
+    startPath: '/dist/index.html',
+  });
+
+  gulp.watch('src/*.scss', ['styles']);
+  gulp.watch('*.pug', ['views']);
+
+  gulp.watch([
+    'dist/**',
+  ])
+    .on('change', browserSync.reload);
+});
