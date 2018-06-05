89b916f01dfbf03e63bb16f272caa5cbc6b69b61
bernardo de stefano <bernardo.destefano@gmail.com>
2018-06-05 16:38:21 +0200

    add pug, gulp config improvements

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,37 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const pug = require('gulp-pug');
 const path = require('path');
 const sasslint = require('gulp-sass-lint');
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
+const header = require('gulp-header');
+
+const pkg = require('./package.json');
 
 const isProduction = process.env.NODE_ENV === 'production';
+
+function getBanner() {
+return `/**
+* ${pkg.name} - v${pkg.version}
+* ${pkg.description}
+* Build ${new Date().toISOString()}
+*/
+`;
+} // getBanner
+
+/** VIEW */
+gulp.task('views', () =>
+  gulp.src('index.pug')
+    .pipe(pug({
+      pretty: !isProduction,
+      data: {
+        environment: isProduction ? 'production' : 'development',
+      },
+    }))
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest('dist')),
+);
 
 /** LINT STYLES */
 gulp.task('lint-styles', () =>
@@ -20,14 +46,16 @@ gulp.task('styles', ['lint-styles'], () => {
   
   if(!isProduction) {
     return cssBundle
+      .pipe(header(getBanner()))
       .pipe(gulp.dest('./dist'))
   }
 
   return cssBundle
     .pipe(cleanCSS())
+    .pipe(header(getBanner()))
     .pipe(rename({ basename: 'flexy.min' }))
     .pipe(gulp.dest('./dist'));
 });
 
 
-gulp.task('default', ['styles']);
+gulp.task('default', ['views','styles']);
